9f9b628bf5c2b86836b295baec72dbab992fff9e
samrouth1234 <bichsamroth@gmail.com>
2025-06-12 14:47:52 +0700

    remove configChart

--- src/app/(front-end)/dashboard/(components)/chart-bar-stacked.tsx
+++ src/app/(front-end)/dashboard/(components)/chart-bar-stacked.tsx
@@ -9,7 +9,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import {
-  ChartConfig,
   ChartContainer,
   ChartLegend,
   ChartLegendContent,
--- src/app/(front-end)/dashboard/(components)/chart-bar.tsx
+++ src/app/(front-end)/dashboard/(components)/chart-bar.tsx
@@ -9,7 +9,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import {
-  ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
